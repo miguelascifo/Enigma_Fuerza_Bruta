b7ca2b2402a24a9023cfbef7d49063f307a3e8fb
Miguel Angel Sanchez Cifo <miguelssanchez@gmail.com>
2017-10-30 19:09:28 +0100

    Brute force working without stickers

--- src/Enigma/Main.java
+++ src/Enigma/Main.java
@@ -1,64 +1,35 @@
 package Enigma;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+        String texto = "GSUTUBBWAXCANFJPPQRLDQQWDJTSVEXHUDHS";
+        String[] diccionario = {"ambiguo","obvio","trivial","estupendo","esther","bugzilla","thevenin","pacifico","diarrea","hola","mundo","garabata","papiloma","herpes","celula","porro","suaves","dimitri","fiesta","patata"};
+        String etiquetas = "AA";
 
-        Scanner sc = new Scanner(System.in);
-        StringBuilder resultado = new StringBuilder();
+        for (char rI = 'A'; rI <= 'Z'; rI++) {
+            for (char rII = 'A'; rII <= 'Z'; rII++) {
+                for (char rIII = 'A'; rIII <= 'Z'; rIII++) {
 
-        System.out.println("Estas usando un simulador Enigma con soporte para 3 rotores, reflector B y etiquetas\n");
+                    StringBuilder resultado = new StringBuilder();
 
-        System.out.println("Escribe la clave pulsando la tecla Intro despues de cada letra. Orden: Rotor I, Rotor II, Rotor III. Ejemplo: C[Intro]H[Intro]S");
-        String rI = sc.nextLine();
-        String rII = sc.nextLine();
-        String rIII = sc.nextLine();
+                    Enigma enigma = new Enigma(new Rotor(Constantes.rotorI, rI, Constantes.saltoI),
+                            new Rotor(Constantes.rotorII, rII, Constantes.saltoII),
+                            new Rotor(Constantes.rotorIII, rIII, Constantes.saltoIII),
+                            new Reflector(),
+                            new Etiquetas(etiquetas));
 
-        System.out.println("Escribe el texto que quieres cifrar.");
-        String texto = sc.nextLine();
+                    for (int i = 0; i < texto.length(); i++) {
+                        resultado.append(enigma.caracterCifrado(texto.charAt(i)));
+                    }
 
-        System.out.println("Escribe las etiquetas que quieres tener cambiadas. Dejalo en blanco si no las quieres cambiar. Ejemplo: AM;HE;QV");
-        String etiquetas = sc.nextLine();
-
-        if (etiquetas.equals("")) {
-            etiquetas = "AA";
+                    for (String aDiccionario : diccionario) {
+                        if (resultado.toString().toLowerCase().contains(aDiccionario)) {
+                            System.out.println(rI + "" + rII + "" + rIII + ": " + resultado.toString());
+                        }
+                    }
+                }
+            }
         }
-
-        // Se comprueba que no se han usado los caracteres ñ, espacio o en blanco en el texto a cifrar
-        if (texto.matches("(.*)Ñ(.*)") || texto.matches("(.*)ñ(.*)") || texto.matches("Ñ(.*)") || texto.matches("ñ(.*)") || texto.matches("(.*)Ñ") || texto.matches("(.*)ñ")) {
-            System.out.println("El texto que has escrito contiene la letra Ñ, que no es soportada. Escribe el texto de nuevo.");
-            texto = sc.nextLine();
-        } else if (texto.matches("(.*) (.*)") || texto.matches(" (.*)") || texto.matches("(.*) ") || texto.matches(" ")) {
-            System.out.println("El texto que has escrito contiene uno o mas espacios. Debes eliminarlos para continuar. Escribe el texto de nuevo.");
-            texto = sc.nextLine();
-        } else if (texto.matches("")) {
-            System.out.println("No has escrito nada. Escribe el texto ahora.");
-            texto = sc.nextLine();
-        }
-
-        // Se convierte a texto en mayúsculas
-        rI = rI.toUpperCase();
-        rII = rII.toUpperCase();
-        rIII = rIII.toUpperCase();
-        texto = texto.toUpperCase();
-        etiquetas = etiquetas.toUpperCase();
-
-        // Se crea el objeto Enigma con la configuración predeterminada
-        Enigma enigma = new Enigma(new Rotor(Constantes.rotorI, rI.charAt(0), Constantes.saltoI),
-                new Rotor(Constantes.rotorII, rII.charAt(0), Constantes.saltoII),
-                new Rotor(Constantes.rotorIII, rIII.charAt(0), Constantes.saltoIII),
-                new Reflector(),
-                new Etiquetas(etiquetas));
-
-        // Se pasa cada carácter por el simulador Enigma
-        for (int i = 0; i < texto.length(); i++) {
-            resultado.append(enigma.caracterCifrado(texto.charAt(i)));
-        }
-
-        System.out.println("\n******** TEXTO CIFRADO ********");
-        System.out.println(resultado);
-        System.out.println("*******************************");
     }
 }
